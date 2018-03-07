8e794d70be663bcfc53abcc03a9f604b7b58dfc8
rjs1197 <gmj1197@gmail.com>
2018-03-07 17:47:43 +0900

    docs: Add, Solve #subject1
    Algorithm name "팩토리얼의 재귀적 방법과 반복적 방법".

---
+++ univ/subject1.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+double fec1(int n);
+double fec2(int n);
+
+void main(void)
+{
+	int n;
+	scanf("%d", &n);
+
+	clock_t start, finish;
+	start = clock();
+	
+	fec1(n); // 재귀적 방법
+	
+	finish = clock();
+	
+	double duration;
+	duration = (double)(finish - start)/CLOCKS_PER_SEC;
+	
+	printf("재귀적 방법 결과 값: %e, %f 초입니다.\n", fec1(n), duration);
+	start = clock();
+
+	fec2(n); // 반복적 방법
+
+	finish = clock();
+
+	duration = (double)(finish - start)/CLOCKS_PER_SEC;
+
+	printf("반복적 방법 결과 값: %e, %f 초입니다.\n", fec2(n), duration);
+}
+
+double fec1(int n)
+{
+	if(n > 1)
+		return n * fec1(n-1);
+	else
+		return 1;
+}
+
+double fec2(int n)
+{
+	double a = 1.0;
+	for(int i = n; i > 1; i--)
+	{
+		a *= i;
+	}
+
+	return a;
+}
+
