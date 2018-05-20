8a3212533de7ebdf15f55c1d1a7b74860bc96de1
rjs1197 <gmj1197@gmail.com>
2018-05-20 22:27:38 +0900

    docs: Sol univ - k-napsack
    Solve algorithm, name is '0-1 knapsack(배낭)'.

--- univ/knapsack.cpp
+++ univ/knapsack.cpp
@@ -1,44 +1,43 @@
 #include <iostream>
 using namespace std;
 
-#define N 4
-#define W 16
+#define W 17
+const int n = 4;
 
-void knapsack(int i, int maxprofit, int weight);
-bool promising(int i);
+void knapsack(int i, int profit, int weight);
+bool promising(int i, int profit, int weight);
 
-char including[N] = {0, };
-char bestset[N] = {0, };
+char including[n] = {0, };
+char bestset[n] = {0, };
 
-int p[N] = {40, 30, 50, 10};
-int w[N] = {2, 5, 10, 5};
+int p[n] = {40, 30, 50, 10};
+int w[n] = {2, 5, 10, 5};
 
-int profit = 0;
-int weight = 0;
 int numbest = 0;
 int maxprofit = 0;
 
 int main(void)
 {
+	knapsack(0, 0, 0);
 
-	knapsack(0, maxprofit, weight);
-
-	for (int j = 0; j <= numbest; j++){
+	for (int j = 0; j < n; j++) 
+	{
 		cout << bestset[j];
 	}
 	cout << endl;
-	
+
 	return 0;
 }
 
-void knapsack(int i, int maxprofit, int weight)
+void knapsack(int i, int profit, int weight)
 {
-	if(weight <= W && profit > maxprofit){
+	if (weight <= W && profit > maxprofit) {
 		maxprofit = profit;
 		numbest = i;
-		bestset[i] = including[i];
+		copy(including, including + n, bestset);
 	}
-	if(promising(i)){
+
+	if (promising(i, profit, weight)) {
 		including[i + 1] = 'Y';
 		knapsack(i + 1, profit + p[i + 1], weight + w[i + 1]);
 		including[i + 1] = 'N';
@@ -46,27 +45,23 @@ void knapsack(int i, int maxprofit, int weight)
 	}
 }
 
-bool promising(int i)
+bool promising(int i, int profit, int weight)
 {
-	if(weight >= W)
-		return false;
-	else{
+	if (weight >= W) return false;
+	else
+	{
 		int j = i + 1;
-		float bound = profit;
-		int totweight = weight;
-
-		while(j <= N && totweight + w[j] <= W)
+		float bound = (float)profit;
+		int totweight;
+		while (j <= n && totweight + w[j] <= W)
 		{
 			totweight = totweight + w[j];
-			bound = bound + p[j];
+			bound += p[j];
 			j++;
 		}
-
 		int k = j;
-		if(k <= N)
-			bound = bound + (W - totweight) * p[k] / w[k];
-		
-		return bound > maxprofit;
+		if (k <= n) bound = bound + (W - totweight) * (p[k] / w[k]);
+		return (bound > maxprofit);
 	}
 }
 
