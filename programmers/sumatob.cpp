e5e842a6b5b485dfc0919698f07f3bf7035c5f91
rjs1197 <gmj1197@gmail.com>
2018-05-31 21:32:27 +0900

    feat: Add programmers algorithm - sumatob
    Solve programmers algorithm content - 정수 A부터 B까지의 합

---
+++ programmers/sumatob.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+long long solution(int a, int b) {
+    long long answer = 0;
+    
+    if (a == b)
+        return (long long)a;
+    
+    else if (a < b)
+        for (int i = a; i <= b; i++)
+            answer += i;
+    else
+        for (int i = b; i <= a; i++) 
+            answer += i;
+    
+    return answer;
+}
